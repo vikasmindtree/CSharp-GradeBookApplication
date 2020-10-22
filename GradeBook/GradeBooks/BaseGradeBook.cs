69a42a3a47f7129cdd9140c9bfb42342ac22ccf2
Vikas Surve <Vikas.Surve@mindtree.com>
2020-10-22 17:15:26 +0530

    no message

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,9 +14,11 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
-        public BaseGradeBook(string name)
+        public bool IsWeighted { get; set; }
+        public BaseGradeBook(string name,bool isWeighted)
         {
             Name = name;
+            IsWeighted = IsWeighted;
             Students = new List<Student>();
         }
 
